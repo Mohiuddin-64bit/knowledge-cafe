454c6c4e91983c1a51f36fa762bac53d1e336b3f
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-03-31 07:44:01 +0600

    single Blog added

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import SingleBlog from "../SingleBlog/SingleBlog";
+
+const Blog = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("Blogs.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+  return (
+    <div>
+      <div className="w-[90%] mx-auto mt-8">
+        <div className="grid md:grid-cols-6 gap-8">
+          <div className="md:col-span-4 mb-8">
+            <div className="grid grid-cols-1 gap-4">
+              {data.map((singleData) => (
+                <SingleBlog key={singleData.id} singleData={singleData} />
+              ))}
+            </div>
+          </div>
+          <div className="md:col-span-2 border rounded-md">
+            {/* <SideCart cartData={cartData} />
+            <ToastContainer /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const SingleBlog = (props) => {
+  const { photo, author, userPhoto, title, time } = props.singleData;
+
+  return (
+    <div>
+      <div className="card card-compact w-[800px] mx-auto bg-base-100 shadow-xl">
+        <figure>
+          <img className="" src={photo} alt="" />
+        </figure>
+        <div className="card-body">
+          <div className="flex justify-between">
+            <div className="flex">
+              <img className=" w-10 h-10 rounded-full" src={userPhoto} alt="" />
+              <div className="px-2">
+                <h2 className="card-title ">{author}</h2>
+                <p className="text-[12px] ml-2 text-gray-400">
+                  Mar 14 (4 Days ago)
+                </p>
+              </div>
+            </div>
+            <div>
+              <p>5 min read</p>
+            </div>
+          </div>
+          <h2 className="card-title text-3xl">{title}</h2>
+          <div>
+            <p className="text-gray-500 cursor-pointer">
+              #beginners #Programming
+            </p>
+          </div>
+          <div className="card-actions">
+            <a href="" className="text-blue-400 font-bold my-3">
+              Mark as read
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleBlog;
